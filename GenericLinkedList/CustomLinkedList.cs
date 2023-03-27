7e648682b65570954c78157803509008dd93f779
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-27 19:04:30 +0530

    [Aniket]- perform custom linkedlist using generics

---
+++ GenericLinkedList/CustomLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CustomLinkedList
+{
+    internal class CustomLinked<T>
+    {
+        public Node<T> head;
+
+        internal void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
+        //displaying the data from linked list
+        internal void Display()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- GenericLinkedList/Program.cs
+++ GenericLinkedList/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace GenericLinkedList
+﻿using CustomLinkedList;
+
+namespace GenericLinkedList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Problem!");
+            Console.WriteLine("\n1.CustomLinkedList");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    CustomLinked<int> customLinked = new CustomLinked<int>();
+                    Console.WriteLine("Creating a linked list to add data");
+                    customLinked.Add(20);
+                    customLinked.Add(30);
+                    customLinked.Add(40);
+                    customLinked.Display();
+                    break;
+                    Console.ReadKey();
+            }
         }
     }
 }
