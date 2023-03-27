6e663a3cfd14e24e3eb34afef2ac68dedbb60047
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-28 00:55:14 +0530

    [Aniket]- perform Inbuild Linked Queue

---
+++ InBuildQueue/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace InBuildQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Perfom Inbuild Queue");
+
+            // Create Operation
+            Queue<int> intQueue = new Queue<int>();
+            
+            intQueue.Enqueue(5);
+            intQueue.Enqueue(10);
+            intQueue.Enqueue(15);
+            intQueue.Enqueue(20);
+            intQueue.Enqueue(25);
+
+            // Read Operation
+            Console.WriteLine("Data in Queue is ");
+            foreach (var data in intQueue)
+            {
+                Console.WriteLine(data);
+            }
+
+            //Delete operation
+            Console.WriteLine("after Dequeue operation " + intQueue.Dequeue());
+
+            //Peek operation
+            Console.WriteLine("Peak Element is " + intQueue.Peek()); 
+
+            Console.WriteLine("Data in Queue after Dequeue is ");
+            foreach (var data in intQueue) // print data in queue after all operation
+            {
+                Console.WriteLine(data);
+            }
+
+        }
+    }
+}
